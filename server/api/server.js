d84a617b969240abf1c621cc71f5e5b6d24cef04
cahaug <caleahaug@gmail.com>
2019-09-18 17:13:43 -0700

    creating tables now for server

--- server/api/server.js
+++ server/api/server.js
@@ -4,12 +4,15 @@ const helmet = require('helmet');
 require('dotenv').config();
 
 // routers=require('') go here
+const authRouter = require('../auth/auth-router.js')
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+server.use('/auth/', authRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'OK running'});
