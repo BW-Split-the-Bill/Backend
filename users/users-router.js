c9f391a3bf0ceee83b8140fdab784764b4786f6b
cahaug <caleahaug@gmail.com>
2019-09-19 20:05:53 -0700

    removed hashed passwords from return of all users

--- users/users-router.js
+++ users/users-router.js
@@ -4,11 +4,11 @@ const jwt = require('jsonwebtoken');
 const db = require('../database/dbConfig.js');
 const restricted = require('../middleware/restricted.js');
 
-
+// returns a master list of all users in the database
 usersRouter.get('/all', restricted, async (req, res) => {
     // if the user has admin in header as privilege, AND is logged in, they can see all users 
     if(req.headers.privilege === 'admin') {
-        const authDb = await db('auth')
+        const authDb = await db('auth').select('userId', 'username');
         res.status(200).json(authDb);        
     } else {
         res.status(401).send({message:"Invalid Request"});
