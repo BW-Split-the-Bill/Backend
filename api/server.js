5071a6c6a877540129a420f0ecce940fa285eef8
cahaug <caleahaug@gmail.com>
2019-09-19 19:44:02 -0700

    added a way to see all created user accounts

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 // routers=require('') go here
 const authRouter = require('../auth/auth-router.js')
+const usersRouter = require('../users/users-router.js')
 
 const server = express();
 
@@ -13,6 +14,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/auth/', authRouter);
+server.use('/users/', usersRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({message: 'OK running'});
---
+++ users/users-router.js
@@ -0,0 +1,19 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../database/dbConfig.js');
+const restricted = require('../middleware/restricted.js');
+
+
+usersRouter.get('/all', restricted, async (req, res) => {
+    // if the user has admin in header as privilege, AND is logged in, they can see all users 
+    if(req.headers.privilege === 'admin') {
+        const authDb = await db('auth')
+        res.status(200).json(authDb);        
+    } else {
+        res.status(401).send({message:"Invalid Request"});
+    };    
+});
+
+
+module.exports = usersRouter;
