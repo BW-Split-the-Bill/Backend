73705e9606e13bce7ec03efd41b5cd32b4b42444
cahaug <caleahaug@gmail.com>
2019-09-29 16:44:55 -0700

    implemented testing in test db

--- knexfile.js
+++ knexfile.js
@@ -20,14 +20,14 @@ module.exports = {
     testing: {
       client: 'sqlite3',
       connection: {
-        filename: './database/database.db3',
+        filename: './database/test.db3',
       },
       useNullAsDefault: true,
       migrations: {
-        directory: './data/migrations',
+        directory: './database/migrations',
       },
       seeds: {
-        directory: './data/seeds',
+        directory: './database/seeds',
       },
     },
   };
